0e0696b858e89dea52d64d7dc8c3512c928a7ce5
mariireynoso <fernando6reynoso@hotmail.com>
2018-04-06 11:09:12 -0300

    main ejer1

--- MAIN.py
+++ MAIN.py
@@ -5,34 +5,45 @@ import datetime
 a=Persona()
 b=Datos()
 
-nombre=input("INGRESE UN NOMBRE: ")
-a.setNombre(nombre)
 
 
-apellido=input("INGRESE UN APELLIDO: ")
-a.setApellido(apellido)
+variable1='S'
+
+while variable1 == 'S':
+
+    nombre=input("INGRESE UN NOMBRE: ")
+    a.setNombre(nombre)
 
 
-fecha_nac=input("INGRESE UNA FECHA: ")
-a.setFecha_nac(fecha_nac)
+    apellido=input("INGRESE UN APELLIDO: ")
+    a.setApellido(apellido)
+
+
+    fecha_nac=input("INGRESE UNA FECHA DE NACIMIENTO: ")
+    a.setFecha_nac(fecha_nac)
+
+    altura=input("INGRESE UNA ALTURA: ")
+    b.setAltura(altura)
+
+    peso=input("INGRESE UN PESO: ")
+    b.setPeso(peso)
+
+    fechaRegistro=input("INGRESE LA FECHA DE HOY: ")
+    b.setfechaRegistro(fechaRegistro)
+
+
+    variable = input ("quiere agregar otro registro?")
+
+    if variable=='N':
+        break
 
 
 
-fecha=input("INGRESE UNA FECHA DE PESO: ")
+
+fecha = input("INGRESE UNA FECHA QUE QUIERA SABER EL PESO: ")
 b.setFecha(fecha)
 
 
-altura=input("INGRESE UNA ALTURA: ")
-b.setAltura(altura)
-
-peso=input("INGRESE UN PESO: ")
-b.setPeso(peso)
-
-
-
-print(a.nombre)
-print(a.apellido)
-print(a.fecha_nac)
-print(b.fecha)
-print(b.altura)
-print(b.peso)
+for i in a.registro:
+    if fecha == a.registro[i]:
+        print (i)
